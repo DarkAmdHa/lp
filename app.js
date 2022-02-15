fe9fa7349ede71abdb4afa49ab02518fa67b83c8
Hamid <hamidtahir2000@gmail.com>
2022-02-15 22:11:25 +0500

    Modifications in order to make landing page more presentable on Desktop

--- app.js
+++ app.js
@@ -43,33 +43,7 @@ document.querySelector(".contact-us").addEventListener("click", (e) => {
   }
 });
 
-document.querySelector('#email_signup').addEventListener('submit', e=>{
-  e.preventDefault();
 
-  console.log('asd')
-
-  // const initiateSend = function(){
-  //   var settings = {
-  //       "async": true,
-  //       "crossDomain": true,
-  //       "url": "https://manage.kmail-lists.com/ajax/subscriptions/subscribe",
-  //       "method": "POST",
-  //       "headers": {
-  //       "content-type": "application/x-www-form-urlencoded",
-  //       "cache-control": "no-cache"
-  //       },
-  //       "data": {
-  //       "g": "TksrLy",
-  //       // "$fields": "$first_name,$last_name,Favorite Color,Favorite Book,$source",
-  //       "email": `${document.querySelector('#k_id_email').innerText}`,
-  //       "$source": "Custom Form"
-  //       }
-  //   }
-  //   $.ajax(settings).done(function (response) {
-  //       console.log(response);
-  //   });
-  //   }
-})
 
 
 // Live Chat
